4b0658372c39c739c54ebf5453f6cf965450f370
Paul Bissex <paul@bissex.net>
2010-10-22 22:21:11 -0400

    Basic UI working.

--- tictactoe.py
+++ tictactoe.py
@@ -7,57 +7,82 @@ import random
 
 class Board(object):
 	"""A Tic-Tac-Toe board."""
-	
+
 	def __init__(self):
 		self.cells = [None] * 9
-		
+
+	def __str__(self):
+		map = { None: ".", 'X': "X", 'O': "O" }
+		row1 = " | ".join(map[c] for c in self.cells[0:3])
+		row2 = " | ".join(map[c] for c in self.cells[3:6])
+		row3 = " | ".join(map[c] for c in self.cells[6:9])
+		board = " %s \n-----------\n %s \n-----------\n %s " % (row1, row2, row3)
+		return board
+
+	def __repr__(self):
+		pieces = [str(c) for c in self.cells]
+		return "".join(pieces)
+
 	def set_cell(self, x, y, piece):
 		"""
 		Set the cell at x, y (0-indexed from top left) to the indicated piece.
 		Piece should be an "X" or "O" character or None. 
 		"""
 		self.cells[x + y * 3] = piece
-	
+
 	def get_cell(self, x, y):
 		"""Return contents of the indicated cell"""
-		return self.cells[x + y * 3]
-			
-	def __str__(self):
-		return str(self.cells)
-		
-	def winner(self):
-		"""Determine the winner -- X or O or None"""
-		# XXX stubbed -- it's over when board is full
-		if all(self.cells):
-			return True
+		try:
+			return self.cells[x + y * 3]
+		except IndexError:
+			raise IndexError, "Out of range: %s %s" % (x, y)
 
-	def best_next_move(self, letter):
-		"""X,Y coordinates of the best next move for given letter."""
-		# XXX stubbed -- random
-		position = (1, 1)
-		while self.get_cell(*position) is not None:
-			position = (random.randint(0, 3), random.randint(0, 3))
-		return position
-		
-	def let_human_move(self, letter):
+	def winner(self):
+		"""
+		Determine the winner if any. Return "X", "O", or None.
+		"""
+		# stubbed -- detects full board
+		return all(self.cells)
+
+	def let_computer_move(self):
+		"""
+		Make next move.
+		"""
+		# stubbed -- random
+		x, y = 1, 1
+		while self.get_cell(x, y) is not None:
+			x, y = random.randint(0, 2), random.randint(0, 2)
+		self.set_cell(x, y, "O")
+
+	def let_human_move(self):
 		"""Ask human where it wants to move, and make the change."""
-		move_string = raw_input("Your move (X Y)? ")
-		x, y = int(move_string.split()[0]), int(move_string.split()[1])
-		self.set_cell(x, y, letter)
-		
+		legal_move = False
+		while not legal_move:
+			try:
+				move = int(raw_input("Your move (1-9)? ")) - 1
+				y = move / 3
+				x = move % 3
+				if self.get_cell(x, y):
+					raise ValueError
+				self.set_cell(x, y, "X")
+				legal_move = True
+			except ValueError:
+				print "Already a piece there."
+			except IndexError:
+				print "No such square."
+
 	def play(self):
 		"""Play a game."""
 		while not self.winner():
-			self.let_human_move("X")
 			print self
-			x, y = self.best_next_move("O")
-			self.set_cell(x, y, "O")
-			print self
+			self.let_human_move()
+			self.let_computer_move()
 
 def play():
 	"""Play the game, interactively."""
 	board = Board()
 	board.play()
+
 
 if __name__ == "__main__":
 	play()
